acf7258d32c4824a8a640bf8afb6958088ca48f5
vorsakha <andreitf.dev@gmail.com>
2021-09-28 11:34:07 -0300

    Layout - base style

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,7 +2,7 @@ import Header from "./Header";
 
 const Layout: React.FC = ({ children }) => {
   return (
-    <div>
+    <div className="font-rubik text-gray-800 bg-gradient-to-r from-yellow-500 to-gray-800 min-h-screen">
       <Header />
       {children}
     </div>
