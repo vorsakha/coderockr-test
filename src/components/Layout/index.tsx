67fefe2f58fe9d445ee04edccf57ee4b3dc9a6dd
vorsakha <andreitf.dev@gmail.com>
2021-09-28 16:44:57 -0300

    Layout - replacing padding for margin in chindren wrapper

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -4,7 +4,7 @@ const Layout: React.FC = ({ children }) => {
   return (
     <div className="font-rubik text-gray-800 bg-gradient-to-r from-yellow-500 to-gray-800 min-h-screen">
       <Header />
-      <div className="mt-24 pt-2">{children}</div>
+      <div className="pt-24">{children}</div>
     </div>
   );
 };
