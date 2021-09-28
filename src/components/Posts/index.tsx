f29576903c209ec7265a308ad512e4258e69bd16
vorsakha <andreitf.dev@gmail.com>
2021-09-28 12:20:54 -0300

    Posts - grid

--- src/components/Posts/index.tsx
+++ src/components/Posts/index.tsx
@@ -3,7 +3,7 @@ import { useCallback, useEffect, useState } from "react";
 import Loading from "../common/LoadingSpinner";
 import PostsLink from "./PostsLink";
 
-const Posts = ({ limit = 10 }: { limit?: number }) => {
+const Posts = ({ limit = 6 }: { limit?: number }) => {
   const [posts, setPosts] = useState<PostTypes[] | null>(null);
 
   const fetchPosts = async () => {
@@ -25,9 +25,17 @@ const Posts = ({ limit = 10 }: { limit?: number }) => {
       {posts === null ? (
         <Loading />
       ) : (
-        <ul>
-          {posts.map((item) => (
-            <li key={item.id}>
+        <ul className="grid grid-cols-6 gap-y-10 py-10">
+          {posts.map((item, idx) => (
+            <li
+              className={`${
+                (idx + 1) % 3 === 0 ? `col-span-4 h-160` : `col-span-3 h-80`
+              }
+              ${
+                (idx + 1) % 3 === 0 && (idx + 1) % 2 === 0 ? `col-start-3` : ``
+              } bg-white w-full`}
+              key={item.id}
+            >
               <PostsLink to={`/post/${item.id}`}>{item.title}</PostsLink>
             </li>
           ))}
