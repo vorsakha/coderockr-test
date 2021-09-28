533fb26b4621e1a52ace04eee569a1a24cb99c78
vorsakha <andreitf.dev@gmail.com>
2021-09-28 09:27:23 -0300

    Layout - Header functionality

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,5 +1,26 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
-  return <div>Header</div>;
+  const handleScrollTop = () => {
+    window.scrollTo(0, 0);
+  };
+
+  return (
+    <div>
+      <div>
+        <Link to="/" onClick={handleScrollTop}></Link>Rockr Blog
+      </div>
+
+      <ul>
+        <li>
+          <Link to="/">Posts</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Header;
