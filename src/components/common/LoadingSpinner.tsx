27b0f455718845e87a3ec4fd21750e2abbf5fb19
vorsakha <andreitf.dev@gmail.com>
2021-09-28 18:13:35 -0300

    LoadingSpinner - add optionality for bottom usage

--- src/components/common/LoadingSpinner.tsx
+++ src/components/common/LoadingSpinner.tsx
@@ -1,8 +1,12 @@
 import { AiOutlineLoading3Quarters as Icon } from "@react-icons/all-files/ai/AiOutlineLoading3Quarters";
 
-const Loading = () => {
+const Loading = ({ bot }: { bot?: boolean }) => {
   return (
-    <div className="flex py-2 px-4 mt-4 no-underline focus:outline-none">
+    <div
+      className={`${
+        bot ? `bottom-10 pb-8` : ``
+      } flex py-2 px-8 mt-8 no-underline focus:outline-none`}
+    >
       <Icon className="text-gray-900 animate-spin text-3xl mx-auto" />
     </div>
   );
