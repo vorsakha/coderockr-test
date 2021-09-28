bc391e2bbbdd629d2895320ec40a883db4f3f7b6
vorsakha <andreitf.dev@gmail.com>
2021-09-28 16:07:39 -0300

    LoadingSpinner - darker color

--- src/components/common/LoadingSpinner.tsx
+++ src/components/common/LoadingSpinner.tsx
@@ -3,7 +3,7 @@ import { AiOutlineLoading3Quarters as Icon } from "@react-icons/all-files/ai/AiO
 const Loading = () => {
   return (
     <div className="flex py-2 px-4 mt-4 no-underline focus:outline-none">
-      <Icon className="text-gray-700 animate-spin text-3xl mx-auto" />
+      <Icon className="text-gray-900 animate-spin text-3xl mx-auto" />
     </div>
   );
 };
