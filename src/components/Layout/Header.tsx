41f759ea4b84be3a94cb2c5a8f0e49a7e88252ad
vorsakha <andreitf.dev@gmail.com>
2021-09-29 08:34:29 -0300

    Header - making responsible

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,22 +1,57 @@
+import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
+import handleBlockScroll from "../../utils/blockScroll";
+import Close from "../common/Close";
+import Menu from "../common/Menu";
 
 const Header = () => {
+  const [openMenu, setOpenMenu] = useState<boolean>(false);
+
   const handleScrollTop = () => {
     window.scrollTo(0, 0);
   };
+
+  useEffect(() => {
+    openMenu ? handleBlockScroll(true) : handleBlockScroll(false);
+  }, [openMenu]);
 
   const location = useLocation();
 
   return (
     <header className="p-1 black text-white shadow fixed w-screen top-0 z-40">
       <div className="px-4 flex flex-row justify-between items-center max-w-6xl mx-auto h-24">
-        <div className="text-4xl font-bold">
+        <div className="xl:text-4xl md:text-3xl text-2xl font-bold">
           <Link to="/" onClick={handleScrollTop}>
             Rockr Blog
           </Link>
         </div>
 
-        <ul className="flex flex-row text-3xl">
+        <div className="sm:hidden">
+          {openMenu ? (
+            <Close onClick={() => setOpenMenu(false)} />
+          ) : (
+            <Menu onClick={() => setOpenMenu(true)} />
+          )}
+        </div>
+
+        {openMenu && (
+          <div className="sm:hidden flex items-center text-center justify-center absolute top-0 right-0 mt-24 w-screen h-screen text-2xl">
+            <ul className="-mt-24 black w-screen min-h-partial flex flex-col justify-center">
+              <li className="py-8">
+                <Link to="/">Posts</Link>
+              </li>
+              <li className="py-8">
+                <Link
+                  to={{ pathname: "/contact", state: { background: location } }}
+                >
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </div>
+        )}
+
+        <ul className="sm:flex hidden flex-row xl:text-3xl md:text-2xl text-xl">
           <li className="px-8">
             <Link to="/">Posts</Link>
           </li>
