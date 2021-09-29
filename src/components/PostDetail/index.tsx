0e60301d95ef4f4911a85878b3a71a8fe00fdef9
vorsakha <andreitf.dev@gmail.com>
2021-09-29 16:21:55 -0300

    PostDetail - adding callback to useEffect

--- src/components/PostDetail/index.tsx
+++ src/components/PostDetail/index.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Loading from "../common/LoadingSpinner";
 import handleDate from "../../utils/formatDate";
@@ -10,39 +10,51 @@ const PostDetail = () => {
 
   const { id } = useParams<{ id: string }>();
 
-  const fetchPosts = async () => {
-    const res = await axios.get(
-      `https://stormy-shelf-93141.herokuapp.com/articles?id=${id}`
-    );
-
-    setPost(res.data[0]);
-  };
-
-  const memoizedCall = useCallback(fetchPosts, [id]);
-
   useEffect(() => {
-    memoizedCall();
-  }, [memoizedCall]);
+    const source = axios.CancelToken.source();
+    let mounted = true;
+
+    const fetchPosts = async () => {
+      try {
+        const res = await axios.get(
+          `https://stormy-shelf-93141.herokuapp.com/articles?id=${id}`
+        );
+
+        if (mounted) {
+          setPost(res.data[0]);
+        }
+      } catch (error) {
+        if (axios.isCancel(error)) {
+          mounted = false;
+        } else {
+          throw error;
+        }
+      }
+    };
+
+    fetchPosts();
+
+    return () => {
+      source.cancel();
+      mounted = false;
+    };
+  }, [id]);
 
   return (
     <div className="flex justify-center items-center min-h-partial flex-wrap md:px-6">
-      {post === null ? (
+      {post === null || post === undefined ? (
         <Loading />
       ) : (
         <div className="relative bg-white max-w-6xl mx-auto shadow-lg md:my-8">
           <div className="flex lg:flex-row flex-col">
-            {!imageLoaded && (
-              <div className="h-full w-full lg:max-w-half bg-gray-500"></div>
-            )}
+            {!imageLoaded && <Loading />}
 
-            <div className="min-w-half h-60 sm:h-80 md:h-96 lg:h-full w-full lg:max-w-half">
-              <img
-                className="w-full h-full object-cover"
-                src={post.imageUrl}
-                alt={post.title}
-                onLoad={() => setImageLoaded(true)}
-              />
-            </div>
+            <img
+              className="min-w-half h-60 sm:h-80 md:h-96 lg:h-full w-full lg:max-w-half object-cover"
+              src={post.imageUrl}
+              alt={post.title}
+              onLoad={() => setImageLoaded(true)}
+            />
 
             <div className="min-w-half lg:max-w-half w-full h-full flex flex-col max-w-4xl self-center items-start px-4 md:px-16 md:mt-16 mt-4">
               <small className="pb-8 text-lg">{handleDate(post.date)}</small>
