9c55f07bf071e7bbf391e0b518e768756652e17e
vorsakha <andreitf.dev@gmail.com>
2021-09-29 07:24:44 -0300

    PostDetails - adding image loading

--- src/components/PostDetail/index.tsx
+++ src/components/PostDetail/index.tsx
@@ -6,6 +6,7 @@ import handleDate from "../../utils/formatDate";
 
 const PostDetail = () => {
   const [post, setPost] = useState<PostTypes | null>(null);
+  const [imageLoaded, setImageLoaded] = useState<boolean>(false);
 
   const { id } = useParams<{ id: string }>();
 
@@ -30,20 +31,28 @@ const PostDetail = () => {
       ) : (
         <div className="relative bg-white max-w-6xl mx-auto shadow my-8">
           <div className="flex">
+            {!imageLoaded && (
+              <div className="h-full w-full max-w-half bg-gray-500"></div>
+            )}
+
             <div className="min-w-half max-w-half object-cover">
-              <img src={post.imageUrl} alt={post.title} />
+              <img
+                src={post.imageUrl}
+                alt={post.title}
+                onLoad={() => setImageLoaded(true)}
+              />
             </div>
 
             <div className="min-w-half max-w-half h-full flex flex-col self-center items-start p-16">
               <small className="pb-8 text-lg">{handleDate(post.date)}</small>
               <p className="p-0 pb-8 text-xl">{post.author}</p>
-              <h1 className="pb-8 text-3xl text-yellow font-bold">
+              <h1 className="pb-8 text-4xl text-yellow font-bold">
                 {post.title}
               </h1>
             </div>
           </div>
 
-          <article className="p-16 paragraph">
+          <article className="py-24 paragraph max-w-3xl mx-auto">
             <div
               dangerouslySetInnerHTML={{
                 __html: post.article,
