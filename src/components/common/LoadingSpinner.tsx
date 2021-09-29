fc27900c0f65866b534111df6e1c59ae80c907da
vorsakha <andreitf.dev@gmail.com>
2021-09-29 16:16:53 -0300

    LoadingSpinner - adding role

--- src/components/common/LoadingSpinner.tsx
+++ src/components/common/LoadingSpinner.tsx
@@ -6,6 +6,7 @@ const Loading = ({ bot }: { bot?: boolean }) => {
       className={`${
         bot ? `bottom-10 pb-8` : ``
       } flex py-2 px-8 mt-8 no-underline focus:outline-none`}
+      role="alert"
     >
       <Icon className="text-gray-900 animate-spin text-3xl mx-auto" />
     </div>
