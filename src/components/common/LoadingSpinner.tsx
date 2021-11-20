123f0f3cd6bfbeb090607557ab065918cee6bb70
vorsakha <andreitf.dev@gmail.com>
2021-11-20 15:22:06 -0300

    add class options to Loading spinner

--- src/components/common/LoadingSpinner.tsx
+++ src/components/common/LoadingSpinner.tsx
@@ -1,14 +1,14 @@
 import { AiOutlineLoading3Quarters as Icon } from "@react-icons/all-files/ai/AiOutlineLoading3Quarters";
 
-const Loading = ({ bot }: { bot?: boolean }) => {
+const Loading = ({ bot, className }: { bot?: boolean; className?: string }) => {
   return (
     <div
       className={`${
         bot ? `bottom-10 pb-8` : ``
-      } flex py-2 px-8 mt-8 no-underline focus:outline-none`}
+      } flex py-2 px-8 mt-8 no-underline focus:outline-none ${className}`}
       role="alert"
     >
-      <Icon className="text-gray-900 animate-spin text-3xl mx-auto" />
+      <Icon className="text-yellow-500 animate-spin text-3xl mx-auto" />
     </div>
   );
 };
