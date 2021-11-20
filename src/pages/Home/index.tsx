604617bd85d885794bc8bab3a8adf75202c2632f
vorsakha <andreitf.dev@gmail.com>
2021-11-20 14:56:53 -0300

    add infiniteScroll hook to home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,43 +1,9 @@
-import { useEffect, useRef, useState } from "react";
 import Posts from "../../components/Posts";
+import useInfiniteScroll from "../../hooks/useInfiniteScroll";
 
 const Home = () => {
-  const [element, setElement] = useState<HTMLElement | null>(null);
-  const [page, setPage] = useState<number>(1);
+  const { setElement, page } = useInfiniteScroll();
 
-  // inf scroll
-  const pageRef = useRef(page);
-
-  useEffect(() => {
-    if (page !== 4) pageRef.current = page;
-  }, [page]);
-
-  const observer = useRef(
-    new IntersectionObserver(
-      (entries) => {
-        const refElement = entries[0];
-        if (refElement.isIntersecting) {
-          setPage(pageRef.current + 1);
-        }
-      },
-      { threshold: 0.8 }
-    )
-  );
-
-  useEffect(() => {
-    const currentElement = element;
-    const currentObserver = observer.current;
-
-    if (currentElement) {
-      currentObserver.observe(currentElement);
-    }
-
-    return () => {
-      if (currentElement) {
-        currentObserver.unobserve(currentElement);
-      }
-    };
-  }, [element]);
   return (
     <div>
       <Posts setElement={setElement} page={page} />
